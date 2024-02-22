3fdb258c11e67e48082269ceb5610fe9b30c84bd
Álvaro Ramírez Lorente <e_arimir@mercadona.com>
2024-02-22 15:54:12 +0100

    Add envs files

---
+++ cypress.config.linux.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require("cypress");
+const cucumber = require('cypress-cucumber-preprocessor').default
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      on('file:preprocessor', cucumber())
+    },
+    defaultCommandTimeout: 5000,
+    specPattern: "cypress/e2e/**/*.{js,ts,feature}",
+    chromeWebSecurity: false,
+    experimentalStudio: true,
+    reporter:'cypress-multi-reporters',
+    reporterOptions: {
+      configFile: 'reporter-config.json'
+    },
+    env: {
+      webdriveruni_homepage: "http://www.webdriveruniversity.com",
+      first_name: "John"
+    }
+  },
+});
